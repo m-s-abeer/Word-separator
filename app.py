f50a2ccb13fd487efae2bf436e9699bd44cb72d7
M-S-Abeer <35918365+M-S-Abeer@users.noreply.github.com>
2019-03-22 01:53:35 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+
+### 1. Preprocess corpus
+### 2. Take queries
+### 3. Run aho-corasick and generate lists of possible words in each end-points
+### 4. Call brute-force for smaller sentences
+### 5. Try randomized algorithm for larger sentences
